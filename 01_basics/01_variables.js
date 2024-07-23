73a8801c8d2c99a54f8124615400b6a9e69ddb20
shivambhati10 <shivambhati990@gmail.com>
2024-07-23 22:19:20 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId= 1445553
+let accountEmail="shivambhati990@gmail.com"
+var accountPassword="12345"
+accountCity="jaipur" //can also be declare like this
+let accouintState;
+//accountId=2 //not allowed
+
+accountEmail="shivam990@gmail.com"
+accountPassword="123423745"
+accountCity="delhi"
+/*
+prefer not to use 
+because of issue in block scope and functional scope
+
+*/
+console.table([accountEmail,accountPassword,accountCity,accouintState])
+
+console.log(accountId)
