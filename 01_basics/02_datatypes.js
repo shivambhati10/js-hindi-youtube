40271b8785392cdca4e2e35ed5b6ac9336a94ab1
shivambhati10 <shivambhati990@gmail.com>
2024-07-23 22:16:51 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,33 @@
+"use strict"; //treat all jks code as newer version 
+//you can avoid semicolon ;
+//alert(3+3)  //we are using nodejs , not browser
+
+console.log(3
+    +
+    3) //code readability should be high
+
+let name="shivam"
+let age = 21
+let isloggedIn= true
+//primitive data types
+
+//number--> 2 to power 53
+//bigInt 
+// string --> ""
+//boolean-->true/false
+
+//null => standalone value mtlb ye bhi ek datatype h
+//null is a representation of empty value
+let state=null
+//undefined =>value is not assigned
+//symbol => for unique 
+
+
+//object
+console.log(typeof"shivam");
+
+console.log(typeof null);  //o/p is object
+console.log(typeof undefined);//o/p is undefined
+
+
+
