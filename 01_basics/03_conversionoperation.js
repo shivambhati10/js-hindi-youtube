17911afb335d40c3b2b32f4159a38f52e7c60954
shivambhati10 <shivambhati990@gmail.com>
2024-07-23 23:11:18 +0000

    conversion

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,26 @@
+let score= null
+
+console.log(typeof score);
+//number ke andar convert kar raha hu
+let valueINnumber = Number(score)
+console.log(valueINnumber);
+
+
+// "33"=> 33 can be converted 
+
+//"33abc" => NaN NOT A NUMBER
+//true => 1; false =>0;
+//boolean ke andar convert kr raha hu
+let isloggedIn=1
+ let booleanIsloggedIn= Boolean(isloggedIn)
+ console.log(booleanIsloggedIn);
+
+ //1 =>true ,0=>false
+ //" "=> false
+ //"shivam"=> true
+
+ //converted this numebr into string
+let someNumber= 33
+ let stringNumber = String(someNumber)
+ console.log(stringNumber)
+ console.log(typeof stringNumber);
