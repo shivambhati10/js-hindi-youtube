8c566f8b5890c4bbffc5031b3e8c296c88d37e47
shivambhati10 <shivambhati990@gmail.com>
2024-07-24 12:49:31 +0000

    conversionoperations

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -1,27 +1,65 @@
-let score= null
+// let score= null
 
-console.log(typeof score);
-//number ke andar convert kar raha hu
-let valueINnumber = Number(score)
-console.log(valueINnumber);
+// console.log(typeof score);
+// //number ke andar convert kar raha hu
+// let valueINnumber = Number(score)
+// console.log(valueINnumber);
 
 
 // "33"=> 33 can be converted 
 
 //"33abc" => NaN NOT A NUMBER
 //true => 1; false =>0;
-//boolean ke andar convert kr raha hu
-let isloggedIn=1
- let booleanIsloggedIn= Boolean(isloggedIn)
- console.log(booleanIsloggedIn);
+// //boolean ke andar convert kr raha hu
+// let isloggedIn=1
+//  let booleanIsloggedIn= Boolean(isloggedIn)
+//  console.log(booleanIsloggedIn);
 
  //1 =>true ,0=>false
  //" "=> false
  //"shivam"=> true
 
  //converted this numebr into string
-let someNumber= 33
- let stringNumber = String(someNumber)
- console.log(stringNumber)
- console.log(typeof stringNumber);
- console.log("shivam")
+// let someNumber= 33
+//  let stringNumber = String(someNumber)
+//  console.log(stringNumber)
+//  console.log(typeof stringNumber);
+//  console.log("shivam")
+
+
+
+//  // operations 
+// let value= 3
+// let negValue= -value
+// console.log(negValue);
+// console.log("shivam");
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+
+// let str1= "hello"
+// let str2= "shivam"
+
+// let str3 = str1+str2
+// console.log(str3);
+// //for these three output will be same as 12
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+"2");
+
+//again something different
+// console.log("1"+2+2);  //output is 122
+//agr shuruwaat m string hai to sabko string ki tarah treat kiya jaayega
+//or agr string last m hai to phle jo operation diya hua h vo ho jaayega then fir string ko add krenge
+
+
+// console.log(1+2+"2");  //output is 32
+
+//it is due to  type conversion which converts the the one data type to another data type ,,,toprimitive operatins
+
+
+console.log(+"");  //output is zer0
+console.log(+true);  //o/p is 1
+
+
