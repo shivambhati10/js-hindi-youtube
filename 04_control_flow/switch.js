51586efafbc310e6b5d79bc5d36f51b6f168d036
shivambhati10 <shivambhati990@gmail.com>
2024-07-27 21:52:43 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,31 @@
+// switch (key){
+//     case value:
+
+//     break;
+
+//     default:
+//         break;
+// }
+const month=4;
+
+switch (month){
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+    case 1:
+        console.log("may");
+        break;
+
+        default:
+            console.log("default case match");
+            break;
+} //ans will be april
