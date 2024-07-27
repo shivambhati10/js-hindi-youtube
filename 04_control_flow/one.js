d7f321e663fdb51cee0436ada413dc637df06395
shivambhati10 <shivambhati990@gmail.com>
2024-07-27 21:52:26 +0000

    one , if else

---
+++ 04_control_flow/one.js
@@ -0,0 +1,40 @@
+// IF
+
+const isUserloggedin=true
+ 
+if(isUserloggedin){
+
+}
+// if(3!=2){
+// true
+//}
+
+//operator--> <,>,<=, ==, != , === ,!== --> for negative sign
+//= --> to assign value, == --> to check value
+// === --> to check for value and datatype, 
+// if(2=="2"){
+//     console.log("executed")
+// } //executed
+// if(2==="2"){
+//     console.log("executed")
+// }//wll not execute
+
+// const balance = 1000
+// if(balance>500 ) console.log("test"); // ek hee line m hee likhna hota h
+
+// /// nesting
+
+// if(true)[
+
+// ]else if(){
+
+// }else if(){
+
+// }
+//logical operator
+const userLoggedIn =true
+const debitcard= true
+if(userLoggedIn&&debitcard){//dono true honi chahiye
+
+    console.log("allow to access")
+}
